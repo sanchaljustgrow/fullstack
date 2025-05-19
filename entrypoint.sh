5c95d53c8013131d569984edf51c6941ee5d5542
sanchaljustgrow <sanchal.khedkar@justgrowinfotech.com>
2025-05-19 17:15:53 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-
-if [ -f "/app/backend.jar" ]; then
-  echo "Starting Backend (Spring Boot)..."
-  java -jar /app/backend.jar
-elif [ -d "/app/dist" ]; then
-  echo "Starting Frontend (Nginx)..."
-  nginx -g "daemon off;"
-else
-  echo "Error: No application to run.  Make sure backend.jar or /app/dist exists."
-  exit 1
-fi
+#!/bin/sh
+java -jar /app/backend.jar &  # Start Spring Boot in the background
+nginx -g "daemon off;"        # Start Nginx in the foreground
